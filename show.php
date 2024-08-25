2d049d8b500e72d045c0af6172430863b720292b
davidayman11 <davidayman2004@gmail.com>
2024-08-26 00:34:38 +0300

    test

--- show.php
+++ show.php
@@ -1,24 +1,16 @@
 <?php
-// Enable error reporting for debugging
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-// Detailed error reporting for mysqli
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$servername = "shamandorascout.com";
+$username = "u968010081_mogamaa";
+$password = "Mogamaa_2000";
+$dbname = "u968010081_mogamaa";
 
-try {
-    // Database connection
-    $servername = "shamandorascout.com"; // or 'localhost' if on the same server
-    $username = "u968010081_mogamaa";
-    $password = "Mogamaa_2000";
-    $dbname = "u968010081_mogamaa";
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-    $conn = new mysqli($servername, $username, $password, $dbname);
-
-    // Retrieve data from the database
-    $sql = "SELECT * FROM employees";
-    $result = $conn->query($sql);
-} catch (mysqli_sql_exception $e) {
-    die("Connection failed: " . $e->getMessage());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+echo "Connected successfully";
 ?>
