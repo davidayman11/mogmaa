cfd45d008a3d7574d7d4e18f0a07a0b0a44b5e95
davidayman11 <davidayman2004@gmail.com>
2024-08-25 18:16:25 +0300

    try

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MOGaM3'24</title>
+    <title>MOGAM3'24</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
