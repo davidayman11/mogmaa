6c2194d090207a943deea90bd0a4564541effec6
davidayman11 <davidayman2004@gmail.com>
2024-08-25 18:12:49 +0300

    try

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Employee Form</title>
+    <title>MOGaM3'24</title>
     <link rel="stylesheet" href="styles.css">
 </head>
 <body>
