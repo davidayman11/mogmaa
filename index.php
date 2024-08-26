7f64399e867dc2d730ab1edec6ba331226a92d96
davidayman11 <davidayman2004@gmail.com>
2024-08-26 22:29:31 +0300

    d

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Enter Employee Details</title>
+    <title>MOGAM3'24</title>
     <style>
         body {
             font-family: 'Arial', sans-serif;
@@ -100,7 +100,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-tool">
               <path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z" />
             </svg>
-            Enter Employee Details</a>
+            MOGAM3'24</a>
         </li>
         <li>
         <a href="./show.php">
@@ -109,39 +109,36 @@
               <polyline points="2 17 12 22 22 17" />
               <polyline points="2 12 12 17 22 12" />
             </svg>
-            Employee Details</a>
+             Details</a>
         </li>
       </ul>
     </nav>
   </div>
   <main class="demo-page-content">
-    <section>
-      <h1>Enter Employee Details</h1>
-      <form action="submit.php" method="post">
-        <div class="nice-form-group">
-          <label>Name:</label>
-          <input type="text" name="name" placeholder="Your name" required />
-        </div>
-        <div class="nice-form-group">
-          <label>Phone:</label>
-          <input type="tel" name="phone" placeholder="Your Phone" required />
-        </div>
-        <div class="nice-form-group">
-          <label>Team:</label>
-          <input type="text" name="team" placeholder="Team" required />
-        </div>
-        <div class="nice-form-group">
-          <label>Grade:</label>
-          <input type="text" name="grade" placeholder="Grade" required />
-        </div>
-        <div class="nice-form-group">
-          <label>Payment:</label>
-          <input type="text" name="payment" placeholder="Payment" required />
-        </div>
-        <input type="submit" value="Submit">
-      </form>
-    </section>
-  </main>
-</div>
-</body>
-</html>
+  <section>
+    <h1>Enter Details</h1>
+    <form action="submit.php" method="post">
+      <div class="nice-form-group">
+        <label>Name:</label>
+        <input type="text" name="name" placeholder="Your name" required />
+      </div>
+      <div class="nice-form-group">
+        <label>Phone:</label>
+        <input type="tel" name="phone" placeholder="Your Phone" value="+2" required />
+      </div>
+      <div class="nice-form-group">
+        <label>Team:</label>
+        <input type="text" name="team" placeholder="Team" required />
+      </div>
+      <div class="nice-form-group">
+        <label>Grade:</label>
+        <input type="text" name="grade" placeholder="Grade" required />
+      </div>
+      <div class="nice-form-group">
+        <label>Payment:</label>
+        <input type="text" name="payment" placeholder="Payment" required />
+      </div>
+      <input type="submit" value="Submit">
+    </form>
+  </section>
+</main>
