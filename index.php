4185b3c7f515cd6797ec22beaec4f0965e6d3cd1
davidayman11 <davidayman2004@gmail.com>
2024-08-28 19:24:34 +0300

    Update index.php

--- index.php
+++ index.php
@@ -120,6 +120,16 @@
             </svg>
             Admin</a>
         </li>
+        <!-- New Logout link -->
+        <li>
+        <a href="./logout.php">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-log-out">
+              <path d="M10 9l-3-3 3-3" />
+              <path d="M21 15v4H3v-4" />
+              <path d="M21 9l-3 3-3-3" />
+            </svg>
+            Logout</a>
+        </li>
       </ul>
     </nav>
   </div>
