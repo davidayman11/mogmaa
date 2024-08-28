b7ac4290d13cffd03a3bbb3725631895b2aee0d9
davidayman11 <davidayman2004@gmail.com>
2024-08-28 19:39:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-// Start the session
-session_start();
 
-// Check if the user is logged in
-if (!isset($_SESSION['logged_in']) || $_SESSION['logged_in'] !== true) {
-    header("Location: login.php"); // Redirect to login page if not logged in
-    exit();
-}
 ?>
 <!DOCTYPE html>
 <html lang="en">
