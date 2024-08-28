96e54e756ca42ea0b78dcb282195d42ce768f110
davidayman11 <davidayman2004@gmail.com>
2024-08-28 19:20:32 +0300

    Update logout.php

--- logout.php
+++ logout.php
@@ -2,19 +2,15 @@
 // Start the session
 session_start();
 
-// Debugging: Display current session data
-// Uncomment the line below to see session contents for debugging
- var_dump($_SESSION); 
-
-// Check if session variables are set
+// Check if the user is logged in
 if (isset($_SESSION['user_id'])) {
-    // Unset all session variables
+    // Unset all of the session variables
     session_unset();
     
     // Destroy the session
     session_destroy();
     
-    // Delete the session cookie if it exists
+    // Optionally, you can also delete the session cookie if it exists
     if (ini_get("session.use_cookies")) {
         $params = session_get_cookie_params();
         setcookie(session_name(), '', time() - 42000,
