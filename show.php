49cc40208b23388fb95da537dc644ec5ac5451eb
davidayman11 <davidayman2004@gmail.com>
2024-09-17 23:51:52 +0300

    Update show.php

--- show.php
+++ show.php
@@ -295,7 +295,7 @@ $is_logged_in = isset($_SESSION['logged_in']) && $_SESSION['logged_in'] === true
               <td><?php echo htmlspecialchars($row["team"]); ?></td>
               <td><?php echo htmlspecialchars($row["grade"]); ?></td>
               <td><?php echo htmlspecialchars(number_format($row["payment"], 2)); ?></td>
-              <td><?php echo htmlspecialchars($row["timestamp"]); ?></td>
+              echo "<td>" . $row["Timestamp"] . "</td>"; // Display Timestamp
 
             </tr>
             <?php endwhile; ?>
