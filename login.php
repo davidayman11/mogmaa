ea15ddbc861fbbcc87916210fdf18302b8164e5d
davidayman11 <129064507+davidayman11@users.noreply.github.com>
2024-11-21 02:48:33 +0200

    Update login.php

--- login.php
+++ login.php
@@ -45,7 +45,7 @@
     <div class="login-container">
         <h1>Login</h1>
         <form action="login_process.php" method="POST">
-            <input type="text" name="userwwwwname" placeholder="Username" required>
+            <input type="text" name="username" placeholder="Username" required>
             <input type="password" name="password" placeholder="Password" required>
             <input type="submit" value="Login">
         </form>
