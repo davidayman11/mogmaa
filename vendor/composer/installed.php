9c7385285ad9a9889258ad892c62330a467862f2
davidayman11 <129064507+davidayman11@users.noreply.github.com>
2025-08-14 17:21:19 +0300

    php

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => '__root__',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => 'c87d16436787c1279014edf509058ee881e03315',
+        'reference' => 'fe18402a6a41e7a929b4201a6e58c3045161d932',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         '__root__' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => 'c87d16436787c1279014edf509058ee881e03315',
+            'reference' => 'fe18402a6a41e7a929b4201a6e58c3045161d932',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
