a1de0d677305b80c8747f2bfb22173fe164ec146
davidayman11 <129064507+davidayman11@users.noreply.github.com>
2025-08-14 17:17:21 +0300

    teams

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInit8b1a85a2cb984dc635045e99735136b2
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInit8b1a85a2cb984dc635045e99735136b2', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInit8b1a85a2cb984dc635045e99735136b2', 'loadClassLoader'));
@@ -32,6 +30,18 @@ class ComposerAutoloaderInit8b1a85a2cb984dc635045e99735136b2
         call_user_func(\Composer\Autoload\ComposerStaticInit8b1a85a2cb984dc635045e99735136b2::getInitializer($loader));
 
         $loader->register(true);
+
+        $filesToLoad = \Composer\Autoload\ComposerStaticInit8b1a85a2cb984dc635045e99735136b2::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
 
         return $loader;
     }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,10 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit8b1a85a2cb984dc635045e99735136b2
 {
+    public static $files = array (
+        '2cffec82183ee1cea088009cef9a6fc3' => __DIR__ . '/..' . '/ezyang/htmlpurifier/library/HTMLPurifier.composer.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'Z' => 
         array (
@@ -65,6 +69,16 @@ class ComposerStaticInit8b1a85a2cb984dc635045e99735136b2
         ),
     );
 
+    public static $prefixesPsr0 = array (
+        'H' => 
+        array (
+            'HTMLPurifier' => 
+            array (
+                0 => __DIR__ . '/..' . '/ezyang/htmlpurifier/library',
+            ),
+        ),
+    );
+
     public static $classMap = array (
         'Composer\\InstalledVersions' => __DIR__ . '/..' . '/composer/InstalledVersions.php',
     );
@@ -74,6 +88,7 @@ class ComposerStaticInit8b1a85a2cb984dc635045e99735136b2
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit8b1a85a2cb984dc635045e99735136b2::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit8b1a85a2cb984dc635045e99735136b2::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit8b1a85a2cb984dc635045e99735136b2::$prefixesPsr0;
             $loader->classMap = ComposerStaticInit8b1a85a2cb984dc635045e99735136b2::$classMap;
 
         }, null, ClassLoader::class);
